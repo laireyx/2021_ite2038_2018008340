01066906a989743ba4b8d3f0410febe27d3ea1aa
miririt <eliotthescarlet@gmail.com>
2021-09-29 00:16:35 +0900

    So DatabaseInstance is moved into types.h. It is more appropriate.

--- project2/filemanager/file.h
+++ project2/filemanager/file.h
@@ -2,26 +2,19 @@
 
 #include "types.h"
 
+/// @brief      Initial number of page count in newly created database file.
+/// @details    It means 10MiB(4B * 2560 = 10MiB).
+constexpr int INITIAL_DATABASE_CAPS = 2560;
+
 /// @brief  Maximum number of database instances count.
 constexpr int MAX_DATABASE_INSTANCE = 1024;
-
-/**
- * @class   DatabaseInstance
- * @brief   Database file instance.
- */
-typedef struct DatabaseInstance {
-    /// @brief Database file path.
-    char* file_path;
-    /// @brief Database file descriptor.
-    int file_descriptor;
-} DatabaseInstance;
 
 /*!
  * @brief   Switch current database into given database.
  * @details If current database_fd is equal to given fd, then do nothing.
  *          If not, change database_fd to given fd and re-read header_page from it.
  *
- * @param fd Database file descriptor gained with file_open_database_file.
+ * @param fd Database file descriptor obtained with file_open_database_file.
  */
 void _switch_to_fd(int fd);
 
@@ -51,7 +44,7 @@ int file_open_database_file(const char* path);
 /**
  * @brief   Allocate an on-disk page from the free page list
  *
- * @param   fd  Database file descriptor gained with file_open_database_file.
+ * @param   fd  Database file descriptor obatined with file_open_database_file.
  * @return      Allocated page index.
  */
 pagenum_t file_alloc_page(int fd);
@@ -59,7 +52,7 @@ pagenum_t file_alloc_page(int fd);
 /**
  * @brief   Free an on-disk page to the free page list
  *
- * @param   fd      Database file descriptor gained with file_open_database_file.
+ * @param   fd      Database file descriptor obatined with file_open_database_file.
  * @param   pagenum page index.
  */
 void file_free_page(int fd, pagenum_t pagenum);
@@ -67,7 +60,7 @@ void file_free_page(int fd, pagenum_t pagenum);
 /**
  * @brief   Read an on-disk page into the in-memory page structure(dest)
  *
- * @param   fd      Database file descriptor gained with file_open_database_file.
+ * @param   fd      Database file descriptor obatined with file_open_database_file.
  * @param   pagenum page index.
  * @param   dest    the pointer of the page data.
  */
@@ -76,7 +69,7 @@ void file_read_page(int fd, pagenum_t pagenum, page_t* dest);
 /**
  * @brief   Write an in-memory page(src) to the on-disk page
  *
- * @param   fd      Database file descriptor gained with file_open_database_file.
+ * @param   fd      Database file descriptor obatined with file_open_database_file.
  * @param   pagenum page index.
  * @param   src     the pointer of the page data.
  */
--- project2/filemanager/types.h
+++ project2/filemanager/types.h
@@ -10,3 +10,14 @@ typedef Page page_t;
 typedef AllocatedPage allocatedpage_t;
 typedef HeaderPage headerpage_t;
 typedef FreePage freepage_t;
+
+/**
+ * @class   DatabaseInstance
+ * @brief   Database file instance.
+ */
+typedef struct DatabaseInstance {
+    /// @brief Real database file path(obtained by realpath(3)).
+    char* file_path;
+    /// @brief Database file descriptor.
+    int file_descriptor;
+} DatabaseInstance;
