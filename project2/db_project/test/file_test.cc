3d806be12ffc70a7328d5cff163ce305d058ab73
박민수 <domece@hanyang.ac.kr>
2021-10-01 11:52:08 +0900

    Configured test documentation

--- project2/db_project/test/file_test.cc
+++ project2/db_project/test/file_test.cc
@@ -1,125 +1,205 @@
 #include "file.h"
 
 #include <gtest/gtest.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include <string>
+#include <cstdlib>
+#include <ctime>
 
-/*******************************************************************************
- * The test structures stated here were written to give you and idea of what a
- * test should contain and look like. Feel free to change the code and add new
- * tests of your own. The more concrete your tests are, the easier it'd be to
- * detect bugs in the future projects.
- ******************************************************************************/
+constexpr int test_count = 128;
 
-/*
- * Tests file open/close APIs.
- * 1. Open a file and check the descriptor
- * 2. Check if the file's initial size is 10 MiB
- */
-TEST(FileInitTest, HandlesInitialization) {
-  int fd;                                 // file descriptor
-  std::string pathname = "init_test.db";  // customize it to your test file
+const char* DATABASE_PATH = "test.db";
+const char* DATABASE_PATH_ALIAS = "./test.db";
+const char* ANOTHER_DATABASE_PATH = "test_another.db";
 
-  // Open a database file
-  fd = file_open_database_file(pathname.c_str());
+class BasicFileManagerTest : public ::testing::Test {
+   protected:
+    // Random indexes for test count
+    int test_order[test_count];
+    int database_fd = 0;
 
-  // Check if the file is opened
-  ASSERT_TRUE(fd >= 0);  // change the condition to your design's behavior
+    BasicFileManagerTest() {
+        database_fd = file_open_database_file(DATABASE_PATH);
+        srand(time(NULL));
 
-  // Check the size of the initial file
-  int num_pages = /* fetch the number of pages from the header page */ 2560;
-  EXPECT_EQ(num_pages, INITIAL_DB_FILE_SIZE / PAGE_SIZE)
-      << "The initial number of pages does not match the requirement: "
-      << num_pages;
+        // Generate random indexes
+        for (int i = 0; i < test_count; i++) {
+            test_order[i] = i + 1;
+        }
 
-  // Close all database files
-  file_close_database_file();
+        for (int i = 0; i < test_count; i++) {
+            int x, y, temp;
+            x = rand() % test_count;
+            y = rand() % test_count;
 
-  // Remove the db file
-  int is_removed = remove(pathname.c_str());
-
-  ASSERT_EQ(is_removed, 0);
-}
-
-/*
- * TestFixture for page allocation/deallocation tests
- */
-class FileTest : public ::testing::Test {
- protected:
-  /*
-   * NOTE: You can also use constructor/destructor instead of SetUp() and
-   * TearDown(). The official document says that the former is actually
-   * perferred due to some reasons. Checkout the document for the difference
-   */
-  FileTest() : pathname("test.db") { fd = file_open_database_file(pathname.c_str()); }
-
-  ~FileTest() {
-    if (fd >= 0) {
-      file_close_database_file();
+            temp = test_order[x];
+            test_order[x] = test_order[y];
+            test_order[y] = temp;
+        }
     }
-  }
-
-  int fd;                // file descriptor
-  std::string pathname;  // path for the file
+    ~BasicFileManagerTest() { file_close_database_file(); }
 };
 
-/*
- * Tests page allocation and free
- * 1. Allocate 2 pages and free one of them, traverse the free page list
- *    and check the existence/absence of the freed/allocated page
+/**
+ * @def     TEST_F(BasicFileManagerTest, HandlesInitialization)
+ * @brief   Tests file open/close APIs.
+ * @details 1. Open a file and check the descriptor
+ *          2. Check if the file's initial size is 10 MiB
  */
-TEST_F(FileTest, HandlesPageAllocation) {
-  pagenum_t allocated_page, freed_page;
+TEST_F(BasicFileManagerTest, HandlesInitialization) {
+    // Check if the file is opened
+    ASSERT_TRUE(database_fd >=
+                0);  // change the condition to your design's behavior
 
-  // Allocate the pages
-  allocated_page = file_alloc_page(fd);
-  freed_page = file_alloc_page(fd);
+    headerpage_t header_page;
+    file_read_page(database_fd, 0, &header_page);
+    int num_pages = header_page.page_num;
+    EXPECT_EQ(num_pages, INITIAL_DB_FILE_SIZE / PAGE_SIZE)
+        << "The initial number of pages does not match the requirement: "
+        << num_pages;
 
-  // Free one page
-  file_free_page(fd, freed_page);
-
-  // Traverse the free page list and check the existence of the freed/allocated
-  // pages. You might need to open a few APIs soley for testing.
-  headerpage_t header_page;
-  file_read_page(fd, 0, reinterpret_cast<page_t*>(&header_page));
-
-  bool is_freed_correctly = false;
-  int current_page_idx = header_page.free_page_idx;
-  freepage_t free_page;
-  while(current_page_idx != 0) {
-    is_freed_correctly = is_freed_correctly || (current_page_idx == freed_page);
-    EXPECT_NE(current_page_idx, allocated_page);
-
-    file_read_page(fd, current_page_idx, reinterpret_cast<page_t*>(&free_page));
-    current_page_idx = free_page.next_free_idx;
-  }
-  EXPECT_TRUE(is_freed_correctly);
-  
-  file_free_page(fd, allocated_page);
+    struct stat database_stat;
+    lstat(DATABASE_PATH, &database_stat);
+    EXPECT_EQ(database_stat.st_size, 4096 * 2560);
 }
 
-/*
- * Tests page read/write operations
- * 1. Write/Read a page with some random content and check if the data matches
+/**
+ * @def     TEST_F(BasicFileManagerTest, HandlesPageAllocation)
+ * @brief   Tests page allocation and free
+ * @details 1. Allocate 2 pages and free one of them, traverse the free page
+ * list and check the existence/absence of the freed/allocated page
  */
-TEST_F(FileTest, CheckReadWriteOperation) {
-    int free_page_num = file_alloc_page(fd);
+TEST_F(BasicFileManagerTest, HandlesPageAllocation) {
+    pagenum_t allocated_page, freed_page;
+
+    // Allocate the pages
+    allocated_page = file_alloc_page(database_fd);
+    freed_page = file_alloc_page(database_fd);
+
+    // Free one page
+    file_free_page(database_fd, freed_page);
+
+    // Traverse the free page list and check the existence of the
+    // freed/allocated pages. You might need to open a few APIs soley for
+    // testing.
+    headerpage_t header_page;
+    file_read_page(database_fd, 0, reinterpret_cast<page_t*>(&header_page));
+
+    // Flag which means if freed page is in free page list correctly.
+    bool is_freed_correctly = false;
+    int current_page_idx = header_page.free_page_idx;
+    freepage_t free_page;
+
+    while (current_page_idx != 0) {
+        is_freed_correctly =
+            is_freed_correctly || (current_page_idx == freed_page);
+
+        // Expect that an allocated page should not be in free page list.
+        EXPECT_NE(current_page_idx, allocated_page);
+
+        file_read_page(database_fd, current_page_idx,
+                       reinterpret_cast<page_t*>(&free_page));
+        current_page_idx = free_page.next_free_idx;
+    }
+    EXPECT_TRUE(is_freed_correctly);
+
+    file_free_page(database_fd, allocated_page);
+}
+
+/**
+ * @def     TEST_F(BasicFileManagerTest, CheckReadWriteOperation)
+ * @brief   Tests page read/write operations
+ * @details 1. Write/Read a page with some random content and check if the data
+ * matches
+ */
+TEST_F(BasicFileManagerTest, CheckReadWriteOperation) {
+    int free_page_num = file_alloc_page(database_fd);
 
     allocatedpage_t page;
     int* page_data = reinterpret_cast<int*>(page.reserved);
 
+    // Generate random values for write
     int random_values[1024] = {};
     for(int i = 0; i < 1024; i++) {
         random_values[i] = rand();
+        // Set that data into allocated page area
         page_data[i] = random_values[i];
     }
 
-    file_write_page(fd, free_page_num, &page);
-    file_read_page(fd, free_page_num, &page);
+    // Write page into file
+    file_write_page(database_fd, free_page_num, &page);
+    // Read page from file
+    file_read_page(database_fd, free_page_num, &page);
 
     for(int i = 0; i < 1024; i++) {
+        // Validation
         EXPECT_EQ(random_values[i], page_data[i]);
     }
-    
-    file_free_page(fd, free_page_num);
+
+    file_free_page(database_fd, free_page_num);
+}
+
+/**
+ * @def     TEST_F(BasicFileManagerTest, UniqueIdTest)
+ * @brief   Tests unique database fd
+ * @details Create database files with different path, but same realpath to
+ * check if database uses unique id for that file. Also checks real different
+ * file and assure that two different database fd should not be same.
+ */
+TEST_F(BasicFileManagerTest, UniqueIdTest) {
+    EXPECT_EQ(database_fd, file_open_database_file(DATABASE_PATH));
+    EXPECT_EQ(database_fd, file_open_database_file(DATABASE_PATH_ALIAS));
+
+    EXPECT_NE(database_fd, file_open_database_file(ANOTHER_DATABASE_PATH));
+}
+
+/**
+ * @def     TEST_F(BasicFileManagerTest, SequentialAllocateTest)
+ * @brief   Tests sequential allocation
+ * @details Check if pages are correctly allocated and freed. it does not
+ * include any expectation, so test procedure purely depends on filemanager's
+ * own error checking method.
+ */
+TEST_F(BasicFileManagerTest, SequentialAllocateTest) {
+    int allocation_result[test_count] = {
+        0,
+    };
+
+    for (int i = 0; i < test_count; i++) {
+        allocation_result[i] = file_alloc_page(database_fd);
+    }
+
+    for (int i = 0; i < test_count; i++) {
+        file_free_page(database_fd, allocation_result[i]);
+    }
+}
+
+/**
+ * @def     TEST_F(BasicFileManagerTest, RandomAllocateTest)
+ * @brief   Tests random allocation
+ * @details This disk space manager uses LIFO for free page management.
+ *          Allocate and free page randomly first then allocate again and check
+ *          if second allocation result is equal to first free result in reverse
+ * order.
+ */
+TEST_F(BasicFileManagerTest, RandomAllocateTest) {
+    for (int i = 0; i < test_count; i++) {
+        file_alloc_page(database_fd);
+    }
+
+    // Randomly free page
+    for (int i = 0; i < test_count; i++) {
+        file_free_page(database_fd, test_order[i]);
+    }
+
+    // Check if the allocated page id is equal to freed page in reverse order.
+    for (int i = 0; i < test_count; i++) {
+        EXPECT_EQ(test_order[test_count - 1 - i], file_alloc_page(database_fd));
+    }
+
+    for (int i = 0; i < test_count; i++) {
+        file_free_page(database_fd, test_order[i]);
+    }
 }
