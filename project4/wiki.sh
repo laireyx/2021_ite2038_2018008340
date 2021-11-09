816f34f517b6b710a9217487d5478627cd394cea
miririt <eliotthescarlet@gmail.com>
2021-11-09 23:04:07 +0900

    Fixed wikigen

--- project4/wiki.sh
+++ project4/wiki.sh
@@ -5,7 +5,7 @@ doxybook2 \
     --input doxygen/xml \
     --output ../../2021_ite2038_2018008340.wiki \
     --config db_project/.doxybook/config.json \
-    --summary-input .doxybook/home.md.tmpl \
+    --summary-input db_project/.doxybook/home.md.tmpl \
     --summary-output ../../2021_ite2038_2018008340.wiki/home.md
 
 cp -r db_project/manual-wiki/* ../../2021_ite2038_2018008340.wiki/
